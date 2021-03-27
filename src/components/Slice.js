3d4f2162d4190c298f2ab5c850b92164e9ba3195
guillermo adrian <gjadriant@gmail.com>
2021-03-27 14:20:57 -0300

    added slice boilerplate

---
+++ src/components/Slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+
+export const Slice = createSlice({
+  name: "",
+  initialState: {
+  },
+  reducers: {
+    add: (state, action) => {
+      return state
+        },
+    remove: (state, action) => {
+      return state
+    },
+    update: (state, action) => {
+      return state
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { remove, add, update } = Slice
+
+export default Slice.reducer
