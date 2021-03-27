a3f10ace5f8b311e6a2d0cf8ab32b255ce1308e3
tatiana talavera <37161374+onecseven@users.noreply.github.com>
2021-03-27 14:08:30 -0300

    Provider is not a default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import Provider from 'react-redux'
+import { Provider } from 'react-redux'
 import store from "./store"
 import './App.css';
 class App extends Component {
